3c4164d7bcdb9d3dedea9506724d59b3937fb6d6
Ek1l <hintl3rgames2015@gmail.com>
2024-03-25 14:10:30 -0300

    Nova conquista Mittre

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
 
-const server = jsonServer.create()
+const server = jsonServer.create();
 
 // Uncomment to allow write operations
 // const fs = require('fs')
@@ -12,20 +12,23 @@ const server = jsonServer.create()
 // const router = jsonServer.router(db)
 
 // Comment out to allow write operations
-const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults()
+const router = jsonServer.router('db.json');
 
-server.use(middlewares)
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
+server.use(
+  jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+    '/blog/:resource/:id/show': '/:resource/:id',
+  }),
+);
+server.use(router);
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
